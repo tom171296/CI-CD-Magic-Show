925353ca6c5978010930a6bf6ab60fc5d29b3234
Tom van den Berg <tom171296@users.noreply.github.com>
2023-12-13 15:40:54 +0100

    dependabot group stuff;

--- src/AutomagicAmusements.Tests/UnitTest1.cs
+++ src/AutomagicAmusements.Tests/UnitTest1.cs
@@ -1,10 +1,10 @@
 namespace AutomagicAmusements.Tests;
 
+[TestClass]
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [TestMethod]
+    public void TestMethod1()
     {
-
     }
 }
